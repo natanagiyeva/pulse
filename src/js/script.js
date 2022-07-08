2c7e4c79c2b83bdbfee3e752bb86099aafebd59d
natanagiyeva <natavan.salayeva@gmail.com>
2022-07-08 22:33:34 +0400

    modal and mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-            breakpoint: 992,
-                settings: {
-                dots: true,
-                arrows: false
-                }
-            }
-        ]
-    });
+    // $('.carousel__inner').slick({
+    //     speed: 1200,
+    //     adaptiveHeight: true,
+    //     prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    //     nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    //     responsive: [
+    //         {
+    //         breakpoint: 992,
+    //             settings: {
+    //             dots: true,
+    //             arrows: false
+    //             }
+    //         }
+    //     ]
+    // });
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
@@ -35,6 +35,25 @@ $(document).ready(function(){
 
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__link_back');
+
+      //Modal
+
+      $('[data-modal=consultation]').on('click', function() {
+            $('.overlay, #consultation').fadeIn('slow');
+    
+        });
+
+        $('.modal1__close').on('click', function() {
+            $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        });
+
+
+        $('.button_mini').each(function(i) {
+            $(this).on('click', function() {
+                $('#order .modal1__descr').text($('.catalog-item__subtitle').eq(i).text());
+                $('.overlay, #order').fadeIn('slow'); 
+            });
+        });
 
 
   });
